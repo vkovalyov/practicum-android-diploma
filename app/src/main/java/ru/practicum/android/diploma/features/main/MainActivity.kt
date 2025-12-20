607bbf9548a5055d5303a8cace365b67346ceec1
Vladimir <89457026+vkovalyov@users.noreply.github.com>
2025-12-21 02:31:52 +0700

    фикс кодстайла

--- app/src/main/java/ru/practicum/android/diploma/features/main/MainActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/main/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.practicum.android.diploma.R
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
